189d7ac9c1fcf54e02a6cd8ea174e5583500a711
Bohdan Slipets <blit@ukr.net>
2025-07-04 15:49:58 +0300

    Виправлено тест: замінено HomePage на MoodSelector

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,14 @@
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:moodmap_clean_new/main.dart';
 
 void main() {
-  testWidgets('App loads and shows HomePage', (WidgetTester tester) async {
+  testWidgets('App loads and shows MoodSelector', (WidgetTester tester) async {
     await tester.pumpWidget(const MoodMapApp());
 
-    expect(find.byType(HomePage), findsOneWidget);
+    // Перевіряємо, що на екрані є текст "MoodMap"
+    expect(find.text('MoodMap'), findsOneWidget);
+
+    // Або можна шукати віджет MoodSelector
+    expect(find.byType(MoodSelector), findsOneWidget);
   });
 }
